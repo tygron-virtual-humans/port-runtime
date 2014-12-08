262698afc5b13d4b559b801dd6380f8c8d3a683c
Vincent Koeman <koeman@chainels.com>
2014-12-08 10:48:42 +0100

    fix UserSpecActionExecutor

--- src/main/java/goal/core/executors/UserSpecActionExecutor.java
+++ src/main/java/goal/core/executors/UserSpecActionExecutor.java
@@ -23,14 +23,12 @@ import goal.core.runtime.service.agent.Result;
 import goal.core.runtime.service.agent.RunState;
 import goal.tools.debugger.Channel;
 import goal.tools.debugger.Debugger;
-import goal.tools.debugger.SteppingDebugger;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-import krTools.language.Query;
 import krTools.language.Substitution;
 import krTools.language.Update;
 import languageTools.program.agent.actions.Action;
@@ -38,34 +36,25 @@ import languageTools.program.agent.actions.UserSpecAction;
 import mentalState.BASETYPE;
 
 public class UserSpecActionExecutor extends ActionExecutor {
-
 	private final UserSpecAction action;
-	private Substitution solution = null;
-	private int indexIntoSpecifications;
 
 	public UserSpecActionExecutor(UserSpecAction act) {
 		this.action = act;
 	}
 
-	/**
-	 * @return The latest substitution from calling evaluatePrecondition
-	 */
-	public Substitution getLatestSubstitution() {
-		return this.solution;
-	}
-
 	@Override
 	public ActionExecutor evaluatePrecondition(MentalState mentalState,
 			Debugger debugger, boolean last) {
-		// Find the first action specification whose precondition holds.
-		final Set<Substitution> solutions = getOptions(mentalState, debugger);
+		MentalStateConditionExecutor check = new MentalStateConditionExecutor(
+				this.action.getPrecondition());
+		final Set<Substitution> solutions = check.evaluate(mentalState,
+				debugger);
 
-		// Return null if no precondition was found that holds; otherwise return
+		// Return null if the precondition did not hold; otherwise return
 		// an instantiated action with the action specification that was found
 		// first for which the precondition holds and use a randomly selected
 		// substitution to instantiate the action.
 		if (solutions.isEmpty()) {
-			// None of the preconditions holds.
 			if (last) {
 				debugger.breakpoint(Channel.ACTION_PRECOND_EVALUATION_USERSPEC,
 						this, "Preconditions of action %s failed.",
@@ -73,92 +62,18 @@ public class UserSpecActionExecutor extends ActionExecutor {
 			}
 			return null;
 		} else {
-			// Select a random substitution that satisfies the precondition we
-			// found.
-			ArrayList<Substitution> substitutions = new ArrayList<>(solutions);
+			// Select a random substitution
+			List<Substitution> substitutions = new ArrayList<>(solutions);
 			Collections.shuffle(substitutions);
 			Substitution solution = substitutions.get(0);
-			UserSpecAction action = getSelectedActionSpec();
-			// Report success.
+			// Report success
 			debugger.breakpoint(Channel.ACTION_PRECOND_EVALUATION_USERSPEC,
-					action, "Precondition { %s } of action %s holds for: %s.",
-					action.getPrecondition(), action.getName(), solution);
-			return new UserSpecActionExecutor(action.applySubst(this.solution));
+					this.action,
+					"Precondition { %s } of action %s holds for: %s.",
+					this.action.getPrecondition(), this.action.getName(),
+					solution);
+			return new UserSpecActionExecutor(this.action.applySubst(solution));
 		}
-	}
-
-	/**
-	 * Searches for the first action specification of this action whose
-	 * precondition holds in the given mental state.
-	 *
-	 * @param mentalState
-	 *            The mental state used for evaluating the precondition.
-	 * @param debugger
-	 *            The current debugger
-	 * @return The solutions, i.e., set of substitutions that satisfy the
-	 *         precondition that was found, or the empty set otherwise.
-	 */
-	protected Set<Substitution> getOptions(MentalState mentalState,
-			Debugger debugger) {
-		Query precondition = null;
-		// Reset index into specifications.
-		this.indexIntoSpecifications = -1;
-
-		// Search for first precondition in list of action specifications that
-		// holds.
-		// We also keep track of the corresponding postcondition.
-		Set<Substitution> solutions = new HashSet<>();
-		for (int i = 0; i < specifications.size(); i++) {
-			PrePost prepost = specifications.get(i);
-			// Get precondition and postcondition.
-			precondition = prepost.getPrecondition();
-			// Check whether precondition holds.
-			solutions = mentalState.query(precondition, BASETYPE.BELIEFBASE,
-					debugger);
-			if (!solutions.isEmpty()) {
-				// We found a precondition that holds; stop searching.
-				// Remember which action specification is satisfied by setting
-				// index into action specifications.
-				this.indexIntoSpecifications = i;
-				break;
-			}
-		}
-		return solutions;
-	}
-
-	/**
-	 * Returns this user-specified action where all action specifications other
-	 * than the one found by {@link #getOptions(MentalState, SteppingDebugger)}
-	 * have been removed.
-	 * <p>
-	 * Only call this AFTER a call to
-	 * {@link #getOptions(MentalState, SteppingDebugger)} .
-	 * </p>
-	 *
-	 * @return This user-specified action where all action specifications other
-	 *         than the one found by
-	 *         {@link #getOptions(MentalState, SteppingDebugger)} have been
-	 *         removed.
-	 */
-	protected UserSpecAction getSelectedActionSpec() {
-		// Check if call to #getOptions(RunState) has been made first.
-		if (this.indexIntoSpecifications == -1) {
-			throw new UnsupportedOperationException(
-					"Calling #getSelectedActionSpec "
-							+ "is only supported after first calling #getOptions(RunState).");
-		}
-
-		// Create new action that only has specification found by
-		// #getOptions(RunState).
-		UserSpecAction action = new UserSpecAction(action.getName(),
-				action.getParameters(), action.getExernal(), null, null,
-				action.getSourceInfo());
-		// Get first action specification found for which precondition holds.
-		PrePost actionspec = specifications.get(this.indexIntoSpecifications);
-		// Do NOT use addSpecification as this renames variables.
-		action.specifications.add(actionspec);
-
-		return action;
 	}
 
 	/**
@@ -175,7 +90,7 @@ public class UserSpecActionExecutor extends ActionExecutor {
 		}
 
 		// Apply the action's postcondition.
-		Update postcondition = specifications.get(0).getPostcondition();
+		Update postcondition = this.action.getPostcondition();
 		runState.getMentalState().insert(postcondition, BASETYPE.BELIEFBASE,
 				debugger);
 
@@ -190,7 +105,6 @@ public class UserSpecActionExecutor extends ActionExecutor {
 
 	@Override
 	protected ActionExecutor applySubst(Substitution subst) {
-		this.solution = subst;
 		return new UserSpecActionExecutor(this.action.applySubst(subst));
 	}
 
