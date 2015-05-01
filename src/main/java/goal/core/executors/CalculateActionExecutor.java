c4160ce566ec45290cda05d80c0d2fa4ed12b515
svenpopping <s.popping@student.tudelft.nl>
2015-05-01 14:02:56 +0200

    Commit before rebase of fork

--- src/main/java/goal/core/executors/CalculateActionExecutor.java
+++ src/main/java/goal/core/executors/CalculateActionExecutor.java
@@ -1,18 +1,24 @@
 package goal.core.executors;
 
+import goal.core.mentalstate.MentalState;
 import goal.core.runtime.service.agent.Result;
 import goal.core.runtime.service.agent.RunState;
 import goal.tools.debugger.Debugger;
 import goal.tools.errorhandling.exceptions.GOALActionFailedException;
 import krTools.language.Substitution;
+import krTools.language.Term;
+import krTools.language.Update;
 import languageTools.program.agent.actions.Action;
+import languageTools.program.agent.actions.CalculateAction;
+
+import java.util.List;
 
 /**
  * Created by svenpopping on 29/04/15.
  */
 public class CalculateActionExecutor extends ActionExecutor {
 
-    private final CalculatenAction action;
+    private final CalculateAction action;
 
     public CalculateActionExecutor(CalculateAction act) {
         this.action = act;
@@ -20,6 +26,14 @@ public class CalculateActionExecutor extends ActionExecutor {
 
     @Override
     protected Result executeAction(RunState<?> runState, Debugger debugger) throws GOALActionFailedException {
+        MentalState mentalState = runState.getMentalState();
+        List<Update> terms = this.action.getParameters();
+
+        Double[] variables = new Double[terms.size() - 2];
+        for (int i = 2; i < terms.size(); i++) {
+            variables[i - 2] = Double.valueOf(terms.get(i).toString());
+        }
+
         return new Result();
     }
 
