e5b712a59ab11d72592867a7aa9b523ce886bbe3
nordinvannes <N.vanNes@student.tudelft.nl>
2015-05-01 11:19:50 +0200

    Multiply operation added to the plugin

---
+++ src/main/Multiply.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.List;
+
+/**
+ * Multiply Class.
+ */
+public class Multiply extends Operation {
+
+    /**
+     * Constructor of multiply class.
+     */
+    public Multiply() {
+        super.setString("Multiply");
+    }
+
+    /**
+     *
+     * @param vars the variables needed for calculation.
+     * @return The answer of the operation.
+     */
+    @Override
+    public final double calc(final List<Double> vars) {
+        if (vars.size() < 2) {
+            throw new ArithmeticException();
+        }
+        return vars.get(0) * vars.get(1);
+    }
+}
