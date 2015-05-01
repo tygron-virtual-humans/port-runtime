9cbb6f583a624dd7b94f95b05a01fb5a9ee5233f
nordinvannes <N.vanNes@student.tudelft.nl>
2015-05-01 11:11:31 +0200

    Updated Operation class to CheckStyle, Plus class accordingly

--- src/main/Operation.java
+++ src/main/Operation.java
@@ -1,7 +1,38 @@
 package main;
 
+import java.util.List;
+
 /**
- * Created by Nordin on 30-4-2015.
+ * Abstract class Operation.
  */
-public class Operation {
+public abstract class Operation {
+
+    /**
+     * private String name.
+     */
+    private String name;
+
+    /**
+     *
+     * @return String name.
+     */
+    public final String getString() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @param newName Name of the operation
+     */
+    public final void setString(final String newName) {
+        this.name = newName;
+    }
+
+    /**
+     *
+     * @param vars the variables needed for calculation.
+     * @return answer in double.
+     */
+    public abstract double calc(final List<Double> vars);
+
 }
--- src/main/Plus.java
+++ src/main/Plus.java
@@ -2,15 +2,27 @@ package main;
 
 import java.util.List;
 
+/**
+ * Plus class.
+ */
 public class Plus extends Operation {
 
+    /**
+     * Constructor.
+     */
     public Plus() {
-        this.name = "plus";
+        setString("plus");
     }
 
-    @Override
-    public double calc(List<Double> list) {
-        if(list.size() < 2) throw new ArithmeticException();
+    /**
+     *
+     * @param list The list of variables for the Plus operation.
+     * @return The answer of the Plus operation.
+     */
+    public final double calc(final List<Double> list) {
+        if (list.size() < 2) {
+            throw new ArithmeticException();
+        }
         return list.get(0) + list.get(1);
     }
 }
