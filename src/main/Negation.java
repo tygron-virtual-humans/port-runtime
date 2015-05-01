4e32aea833205fef5fd56025fe01b8e58e9af077
nordinvannes <N.vanNes@student.tudelft.nl>
2015-05-01 11:12:47 +0200

    Negation operation added.

--- src/main/Negation.java
+++ src/main/Negation.java
@@ -1,7 +1,28 @@
 package main;
 
+import java.util.List;
+
 /**
- * Created by Nordin on 30-4-2015.
+ * Negation class.
  */
-public class Negation {
+public class Negation extends Operation {
+
+    /**
+     * Constructor of the Negation class.
+     */
+    public Negation() {
+        setString("Negation");
+    }
+
+    /**
+     *
+     * @param list variables for the calculation.
+     * @return double is returned with the answer of the operation.
+     */
+    public final double calc(final List<Double> list) {
+        if (list.size() < 1) {
+            throw new ArithmeticException();
+        }
+        return -1 * list.get(0);
+    }
 }
