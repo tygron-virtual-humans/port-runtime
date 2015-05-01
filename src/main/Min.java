4e3ec20351741a48444a57ac7012d20b3fbd6455
nordinvannes <N.vanNes@student.tudelft.nl>
2015-05-01 11:12:07 +0200

    Added Min operation(class)

--- src/main/Min.java
+++ src/main/Min.java
@@ -1,7 +1,29 @@
 package main;
 
+import java.util.List;
+
 /**
- * Created by Nordin on 30-4-2015.
+ * Min class.
  */
-public class Min {
+public class Min extends Operation {
+
+    /**
+     * Constructor of Min class.
+     */
+    public Min() {
+        super.setString("min");
+    }
+
+    /**
+     *
+     * @param list The list of variables for the operation.
+     * @return The answer of the operation.
+     */
+    @Override
+    public final double calc(final List<Double> list) {
+        if (list.size() < 2) {
+            throw new ArithmeticException();
+        }
+        return list.get(0) - list.get(1);
+    }
 }
